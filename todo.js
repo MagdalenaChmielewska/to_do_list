8c7ca3ad9167a01256bef645a2736f3f3479b275
Magda Ch <magdalena.chmielewska.gda@gmail.com>
2017-02-17 12:13:57 +0100

    Initial commit.

---
+++ todo.js
@@ -0,0 +1,81 @@
+function TodoList() {
+    // documentation of Map https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+    var tasks = new Map();
+    var current_id = 0;
+
+    var addTask = function (task) {
+        task.id = current_id
+        tasks.set(current_id, task);
+        current_id += 1;
+        return task;
+    }
+
+    // JSON object
+    addTask({ description: "create todo list", priority: "high" })
+    addTask({ description: "add filtering by priority", priority: "medium" })
+    addTask({ description: "use rest api backend", priority: "low" })
+
+    return {
+        get_task: function (task_id) {
+            return tasks.get(task_id);
+        },
+        add_task: function (task) {
+            return addTask(task);
+        },
+        remove_task: function (task_id) {
+            return tasks.delete(parseInt(task_id));
+        },
+        all_tasks: function () {
+            return tasks.values();
+        },
+        print_all: function () {
+            // for (var i = 0 ; i < tasks.length ; i++)
+            // {
+            //     console.log(tasks[i])
+            // }
+
+            for (var v of tasks) {
+                console.log(v);
+            }
+        }
+    }
+}
+var todoList = new TodoList();
+
+function add() {
+    var description = document.getElementById('task').value;
+
+    todoList.add_task({ "description": description, "priority": "low" })
+
+    show();
+
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    todoList.remove_task(id);
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var html = '<ul>';
+    for (var todo of todoList.all_tasks()) {
+        html += '<li>' + todo.description + '<button class="remove" id="' + todo.id + '">x</button></li>';
+    }
+
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
